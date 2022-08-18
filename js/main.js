784de4ca3b706b5e609db859fbe5748488d088f4
seif <baazouziseif9@gmail.com>
2022-08-18 08:54:46 +0100

    change active link

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ burger.addEventListener("click", () => {
   links.classList.toggle("open")
   burger.classList.toggle("open")
 })
+
+window.onhashchange = () => {
+  const links = document.querySelectorAll("#links a")
+  links.forEach(link => link.classList.remove("active"))
+
+  const hash = document.location.hash
+  let linkID = "home-link"
+  if(hash) {
+    linkID = `${hash.substring(1)}-link`
+  }
+
+  document.getElementById(linkID).classList.add("active")
+
+  links.classList.remove("open")
+  burger.classList.remove("open")
+}
